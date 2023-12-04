203ed5d4e61f3d0fad2942ab7bde4840aac20bf9
eenzzi <purelej99@gmail.com>
2023-12-04 12:04:25 +0900

    View 분리 - v2

--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -1,8 +1,9 @@
-package hello.servlet.web.frontcontroller.v1.controller;
+package hello.servlet.web.frontcontroller.v2.controller;
 
 import hello.servlet.basic.domain.member.Member;
 import hello.servlet.basic.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,12 +11,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-public class MemberSaveContollerV1 implements ControllerV1 {
+public class MemberSaveControllerV2 implements ControllerV2 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
@@ -26,9 +27,8 @@ public class MemberSaveContollerV1 implements ControllerV1 {
         //Model에 데이터를 보관
         request.setAttribute("member", member);
 
-        String viewPath = "/WEB-INF/views/save-result.jsp";
-        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        dispatcher.forward(request,response);;
+
+        return new MyView("/WEB-INF/views/save-result.jsp");
 
     }
 }
